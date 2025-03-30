ff90729bc8f1df304e12165f3b72999e3dfa30a5
MSR25-RegressionBug <brojackcode@gmail.com>
2025-03-31 01:56:15 +1100

    add RegressionBug-34

---
+++ WORKING/src/main/java/com/univocity/parsers/common/DefaultParsingContext.java
@@ -0,0 +1,99 @@
+/*
+ * Copyright (c) 2015 uniVocity Software Pty Ltd. All rights reserved.
+ * This file is subject to the terms and conditions defined in file
+ * 'LICENSE.txt', which is part of this source code package.
+ *
+ */
+package com.univocity.parsers.common;
+
+import com.univocity.parsers.common.input.*;
+
+import java.util.*;
+
+/**
+ * The default {@link ParsingContext} implementation used internally by {@link AbstractParser} to expose information about a parsing process in execution.
+ *
+ * @author uniVocity Software Pty Ltd - <a href="mailto:parsers@univocity.com">parsers@univocity.com</a>
+ * @see com.univocity.parsers.common.ParsingContext
+ * @see com.univocity.parsers.common.AbstractParser
+ * @see com.univocity.parsers.common.processor.RowProcessor
+ */
+public class DefaultParsingContext extends DefaultContext implements ParsingContext {
+
+	private final CharInputReader input;
+
+	private final AbstractParser<?> parser;
+
+
+	public DefaultParsingContext(AbstractParser<?> parser, int errorContentLength) {
+		super(parser == null ? null : parser.output, errorContentLength);
+		this.parser = parser;
+		this.input = parser == null ? null : parser.input;
+	}
+
+	@Override
+	public long currentLine() {
+		return input.lineCount();
+	}
+
+	@Override
+	public long currentChar() {
+		return input.charCount();
+	}
+
+	@Override
+	public void skipLines(long lines) {
+		input.skipLines(lines);
+	}
+
+	@Override
+	public String fieldContentOnError() {
+		char[] chars = output.appender.getChars();
+		if (chars != null) {
+			int length = output.appender.length();
+			if (length > chars.length) {
+				length = chars.length;
+			}
+			if (length > 0) {
+				return new String(chars, 0, length);
+			}
+		}
+		return null;
+	}
+
+	@Override
+	public String currentParsedContent() {
+		if (input != null) {
+			return input.currentParsedContent();
+		}
+		return null;
+	}
+
+	@Override
+	public int currentParsedContentLength() {
+		if (input != null) {
+			return input.currentParsedContentLength();
+		}
+		return 0;
+	}
+
+	@Override
+	public Map<Long, String> comments() {
+		return parser.getComments();
+	}
+
+	@Override
+	public String lastComment() {
+		return parser.getLastComment();
+	}
+
+	@Override
+	public String[] parsedHeaders() {
+		return parser.getParsedHeaders();
+	}
+
+	@Override
+	public char[] lineSeparator() {
+		return input.getLineSeparator();
+	}
+}
---
+++ WORKING/src/main/java/com/univocity/parsers/common/ParsingContextWrapper.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright (c) 2015 uniVocity Software Pty Ltd. All rights reserved.
+ * This file is subject to the terms and conditions defined in file
+ * 'LICENSE.txt', which is part of this source code package.
+ *
+ */
+package com.univocity.parsers.common;
+
+import com.univocity.parsers.common.record.*;
+
+import java.util.*;
+
+/**
+ * A simple a wrapper for a {@link ParsingContext}.
+ *
+ * @author uniVocity Software Pty Ltd - <a href="mailto:parsers@univocity.com">parsers@univocity.com</a>
+ *
+ */
+public class ParsingContextWrapper extends ContextWrapper<ParsingContext> implements ParsingContext {
+
+	/**
+	 * Wraps a {@link ParsingContext}.
+	 * @param context the parsingContext object to be wrapped.
+	 */
+	public ParsingContextWrapper(ParsingContext context) {
+		super(context);
+	}
+
+	@Override
+	public long currentLine() {
+		return context.currentLine();
+	}
+
+	@Override
+	public long currentChar() {
+		return context.currentChar();
+	}
+
+	@Override
+	public void skipLines(long lines) {
+		context.skipLines(lines);
+	}
+
+	@Override
+	public String currentParsedContent() {
+		return context.currentParsedContent();
+	}
+
+	@Override
+	public int currentParsedContentLength() {
+		return context.currentParsedContentLength();
+	}
+
+	@Override
+	public Map<Long, String> comments() {
+		return context.comments();
+	}
+
+	@Override
+	public String lastComment() {
+		return context.lastComment();
+	}
+
+	@Override
+	public String[] parsedHeaders() {
+		return context.parsedHeaders();
+	}
+
+	@Override
+	public char[] lineSeparator() {
+		return context.lineSeparator();
+	}
+
+	@Override
+	public String fieldContentOnError() {
+		return context.fieldContentOnError();
+	}
+
+	@Override
+	public String[] selectedHeaders() {
+		return context.selectedHeaders();
+	}
+
+	@Override
+	public Record toRecord(String[] row) {
+		return context.toRecord(row);
+	}
+}
