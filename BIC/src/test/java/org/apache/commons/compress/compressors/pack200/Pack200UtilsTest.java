402a61c99cf2994148edbb2568558e489a535fe0
MSR25-RegressionBug <brojackcode@gmail.com>
2025-03-31 02:05:15 +1100

    add RegressionBug-96

---
+++ BIC/src/test/java/org/apache/commons/compress/compressors/pack200/Pack200UtilsTest.java
@@ -0,0 +1,95 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.commons.compress.compressors.pack200;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+import java.util.HashMap;
+
+import org.apache.commons.compress.AbstractTest;
+import org.apache.commons.compress.archivers.ArchiveEntry;
+import org.apache.commons.compress.archivers.ArchiveInputStream;
+import org.apache.commons.compress.archivers.ArchiveStreamFactory;
+import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
+import org.apache.commons.compress.archivers.tar.TarArchiveInputStream;
+import org.apache.commons.compress.archivers.tar.TarArchiveOutputStream;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.io.input.CloseShieldInputStream;
+import org.apache.commons.io.output.UnsynchronizedByteArrayOutputStream;
+import org.junit.jupiter.api.Test;
+
+public final class Pack200UtilsTest extends AbstractTest {
+
+    private long parseEntry(final InputStream is) throws IOException {
+        try (UnsynchronizedByteArrayOutputStream bos = UnsynchronizedByteArrayOutputStream.builder().get();
+                Pack200CompressorInputStream p = new Pack200CompressorInputStream(is)) {
+            return IOUtils.copy(p, bos);
+        }
+    }
+
+    @Test
+    public void testNormalize() throws Throwable {
+        final File input = getFile("bla.jar");
+        final File output = createTempFile();
+        Pack200Utils.normalize(input, output, new HashMap<>());
+        try (InputStream is = Files.newInputStream(output.toPath());
+                ArchiveInputStream<?> in = ArchiveStreamFactory.DEFAULT.createArchiveInputStream("jar", is)) {
+            in.forEach(entry -> {
+                final File archiveEntry = newTempFile(entry.getName());
+                archiveEntry.getParentFile().mkdirs();
+                if (entry.isDirectory()) {
+                    archiveEntry.mkdir();
+                } else {
+                    Files.copy(in, archiveEntry.toPath());
+                }
+            });
+        }
+    }
+
+    @Test
+    public void testNormalizeInPlace() throws Throwable {
+        final File input = getFile("bla.jar");
+        final File output = createTempFile();
+        try (InputStream is = Files.newInputStream(input.toPath())) {
+            Files.copy(is, output.toPath(), StandardCopyOption.REPLACE_EXISTING);
+        }
+        Pack200Utils.normalize(output);
+        try (InputStream is = Files.newInputStream(output.toPath());
+                ArchiveInputStream<?> in = ArchiveStreamFactory.DEFAULT.createArchiveInputStream("jar", is)) {
+            in.forEach(entry -> {
+                final File archiveEntry = newTempFile(entry.getName());
+                archiveEntry.getParentFile().mkdirs();
+                if (entry.isDirectory()) {
+                    archiveEntry.mkdir();
+                } else {
+                    Files.copy(in, archiveEntry.toPath());
+                }
+            });
+        }
+    }
+
+}
