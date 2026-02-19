230674d93515ccd8d5c8d293f5f604c6e84400df
brojackvn <anhho.work@gmail.com>
2026-02-19 21:48:47 +1100

    add RegressionBug-125

---
+++ BFC/src/test/java/com/univocity/parsers/fixed/FixedWidthParserTest.java
@@ -0,0 +1,211 @@
+/*******************************************************************************
+ * Copyright 2014 uniVocity Software Pty Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package com.univocity.parsers.fixed;
+
+import com.univocity.parsers.*;
+import com.univocity.parsers.common.processor.*;
+import org.testng.annotations.*;
+
+import java.io.StringReader;
+
+import static org.testng.Assert.assertEquals;
+
+public class FixedWidthParserTest extends ParserTestCase {
+
+	@DataProvider(name = "fileProvider")
+	public Object[][] csvProvider() {
+		return new Object[][]{
+			{".txt", new char[]{'\n'}},
+			{"-dos.txt", new char[]{'\r', '\n'}},
+			{"-mac.txt", new char[]{'\r'}},
+			{".txt", null},
+			{"-dos.txt", null},
+			{"-mac.txt", null}
+		};
+	}
+
+	protected FixedWidthFields getFieldLengths() {
+		return new FixedWidthFields(new int[]{11, 38, 20, 8});
+	}
+
+	private FixedWidthParserSettings newSettings(FixedWidthFields lengths, char[] lineSeparator) {
+		FixedWidthParserSettings settings = new FixedWidthParserSettings(getFieldLengths());
+		if (lineSeparator == null) {
+			settings.setLineSeparatorDetectionEnabled(true);
+		} else {
+			settings.getFormat().setLineSeparator(lineSeparator);
+		}
+		return settings;
+	}
+
+	@Test(enabled = true, dataProvider = "fileProvider")
+	public void testFixedWidthParser(String fileExtension, char[] lineSeparator) throws Exception {
+		FixedWidthParserSettings settings = newSettings(getFieldLengths(), lineSeparator);
+		settings.setHeaderExtractionEnabled(true);
+		settings.setRowProcessor(processor);
+		FixedWidthParser parser = new FixedWidthParser(settings);
+
+		parser.parse(this.newReader("/fixed/essential" + fileExtension));
+
+		String[] expectedHeaders = new String[]{
+			"DATE", "NAME", "OWED", "INTEREST",
+		};
+
+		String[][] expectedResult = new String[][]{
+			{"2013-FEB-28", "Harry Dong", "15000.99", "8.786",},
+			{"2013-JAN-1", "Billy Rubin", "15100.99", "5",},
+			{"2012-SEP-1", "Willie Stroker", "15000.00", "6",},
+			{"2012-JAN-11", "Mike Litoris", "15000", "4.86",},
+			{"2010-JUL-01", "Gaye Males", "1", "8.6",},
+		};
+
+		this.assertHeadersAndValuesMatch(expectedHeaders, expectedResult);
+	}
+
+	@Override
+	protected RowListProcessor newRowListProcessor() {
+		return new RowListProcessor();
+	}
+
+	@Test(enabled = true, dataProvider = "fileProvider")
+	public void testFixedWidthParserSkippingUntilNewLine(String fileExtension, char[] lineSeparator) throws Exception {
+		FixedWidthParserSettings settings = newSettings(getFieldLengths(), lineSeparator);
+		settings.setSkipTrailingCharsUntilNewline(true);
+		settings.setRecordEndsOnNewline(true);
+		settings.setHeaderExtractionEnabled(false);
+		settings.setRowProcessor(processor);
+
+		String[] expectedHeaders = new String[]{
+			"DATE", "NAME", "OWED", "INTEREST",
+		};
+
+		settings.setHeaders(expectedHeaders);
+		FixedWidthParser parser = new FixedWidthParser(settings);
+
+		parser.parse(this.newReader("/fixed/essential_1" + fileExtension));
+
+		String[][] expectedResult = new String[][]{
+			{"2013-FEB-28", "Harry Dong", "15000.99", "8.786",},
+			{"2013-JAN-1", "Billy Rubin", "15100.99", "5",},
+			{"2012-SEP-1", "Willie Stroker"},
+			{"2012-JAN-11", "Mike Litoris", "15000", "4.86",},
+			{"2010-JUL-01", "Gaye Males", "1", "8.6",},
+		};
+
+		this.assertHeadersAndValuesMatch(expectedHeaders, expectedResult);
+	}
+
+	@Test(enabled = true, dataProvider = "fileProvider")
+	public void testFixedWidthParserWithPadding(String fileExtension, char[] lineSeparator) throws Exception {
+		FixedWidthParserSettings settings = newSettings(getFieldLengths(), lineSeparator);
+		settings.getFormat().setPadding('_');
+		settings.setSkipTrailingCharsUntilNewline(true);
+		settings.setRecordEndsOnNewline(true);
+		settings.setHeaderExtractionEnabled(false);
+		settings.setRowProcessor(processor);
+
+		String[] expectedHeaders = new String[]{
+			"DATE", "NAME", "OWED", "INTEREST",
+		};
+
+		settings.setHeaders(expectedHeaders);
+		FixedWidthParser parser = new FixedWidthParser(settings);
+
+		parser.parse(this.newReader("/fixed/essential_3" + fileExtension));
+
+		String[][] expectedResult = new String[][]{
+			{"2013-FEB-28", "Harry Dong", "15000.99", "8.786",},
+			{"2013-JAN-1", "Billy Rubin", "15100.99", "5",},
+			{"2012-SEP-1", "Willie Stroker"},
+			{"2012-JAN-11", "Mike Litoris", "15000", "4.86",},
+			{"2010-JUL-01", "Gaye Males", "1", "8.6",},
+		};
+
+		this.assertHeadersAndValuesMatch(expectedHeaders, expectedResult);
+	}
+
+	@Test(enabled = true, dataProvider = "fileProvider")
+	public void testFixedWidthParserWithPaddingAndNoTrimming(String fileExtension, char[] lineSeparator) throws Exception {
+		FixedWidthParserSettings settings = newSettings(getFieldLengths(), lineSeparator);
+		settings.getFormat().setPadding('_');
+		settings.setSkipTrailingCharsUntilNewline(true);
+		settings.setIgnoreLeadingWhitespaces(false);
+		settings.setIgnoreTrailingWhitespaces(false);
+		settings.setRecordEndsOnNewline(true);
+		settings.setHeaderExtractionEnabled(false);
+		settings.setRowProcessor(processor);
+
+		String[] expectedHeaders = new String[]{
+			"DATE", "NAME", "OWED", "INTEREST",
+		};
+
+		settings.setHeaders(expectedHeaders);
+		FixedWidthParser parser = new FixedWidthParser(settings);
+
+		parser.parse(this.newReader("/fixed/essential_2" + fileExtension));
+
+		String[][] expectedResult = new String[][]{
+			{"2013-FEB-28", "  Harry Dong  ", "15000.99", "  8.786",},
+			{"2013-JAN-1", "Billy Rubin  ", "15100.99", "5",},
+			{"2012-SEP-1", " Willie Stroker"},
+			{"2012-JAN-11", "Mike Litoris ", "15000", "4.86",},
+			{"2010-JUL-01", " Gaye Males ", " 1 ", "8.6  ",},
+		};
+
+		this.assertHeadersAndValuesMatch(expectedHeaders, expectedResult);
+	}
+
+	@Test
+	public void testParsingWithPaddingPerField(){
+		FixedWidthFields fieldLengths = new FixedWidthFields(20,8);
+		fieldLengths.setPadding('0', 1);
+		fieldLengths.setAlignment(FieldAlignment.RIGHT, 1);
+		FixedWidthParserSettings fwws = new FixedWidthParserSettings(fieldLengths);
+
+		fwws.getFormat().setPadding('_');
+		fwws.getFormat().setLineSeparator("\n");
+		fwws.setHeaderExtractionEnabled(true);
+
+		FixedWidthParser parser = new FixedWidthParser(fwws);
+		parser.beginParsing(new StringReader("ziel____________________plzV\nziel0_______________00000000\nziel1_______________00000001\n"));
+
+		assertEquals(parser.parseNext(), new String[]{"ziel0",null});
+		assertEquals(parser.parseNext(), new String[]{"ziel1","1"});
+		assertEquals(parser.getContext().headers(), new String[]{"ziel","plzV"});
+	}
+
+	@Test
+	public void testParsingWithoutRecordBreaks() {
+		int[] length = new int[]{2, 2, 2};
+		FixedWidthFields lengths = new FixedWidthFields(length);
+		FixedWidthParserSettings settings = new FixedWidthParserSettings(lengths);
+
+		FixedWidthParser parser = new FixedWidthParser(settings);
+		parser.beginParsing(new StringReader("abcdefghijkl"));
+
+		String[] data;
+
+		data = parser.parseNext();
+		assertEquals(data[0], "ab");
+		assertEquals(data[1], "cd");
+		assertEquals(data[2], "ef");
+
+		data = parser.parseNext();
+		assertEquals(data[0], "gh");
+		assertEquals(data[1], "ij");
+		assertEquals(data[2], "kl");
+	}
+}
---
+++ BUGGY/src/main/java/com/univocity/parsers/common/DefaultContext.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright (c) 2013 uniVocity Software Pty Ltd. All rights reserved.
+ * This file is subject to the terms and conditions defined in file
+ * 'LICENSE.txt', which is part of this source code package.
+ */
+
+package com.univocity.parsers.common;
+
+/**
+ * Default implementation of the {@link Context} interface with essential information about the output being produced.
+ *
+ * @author uniVocity Software Pty Ltd - <a href="mailto:dev@univocity.com">dev@univocity.com</a>
+ */
+public class DefaultContext implements Context {
+
+	protected boolean stopped = false;
+	final ParserOutput output;
+	final ColumnMap columnMap;
+
+	public DefaultContext(ParserOutput output){
+		this.output = output;
+		this.columnMap = new ColumnMap(this, output);
+	}
+
+	@Override
+	public String[] headers() {
+		return output.getHeaders();
+	}
+
+	public String[] selectedHeaders() {
+		int[] extractedFieldIndexes = extractedFieldIndexes();
+		if (extractedFieldIndexes != null) {
+			String[] extractedFields = new String[extractedFieldIndexes.length];
+			String[] headers = headers();
+			for (int i = 0; i < extractedFieldIndexes.length; i++) {
+				extractedFields[i] = headers[extractedFieldIndexes[i]];
+			}
+			return extractedFields;
+		}
+		return headers();
+	}
+
+	@Override
+	public int[] extractedFieldIndexes() {
+		return output.getSelectedIndexes();
+	}
+
+	@Override
+	public boolean columnsReordered() {
+		return output.isColumnReorderingEnabled();
+	}
+
+	@Override
+	public int indexOf(String header) {
+		return columnMap.indexOf(header);
+	}
+
+	@Override
+	public int indexOf(Enum<?> header) {
+		return columnMap.indexOf(header);
+	}
+
+	void reset() {
+		if(output != null) {
+			output.reset();
+		}
+		columnMap.reset();
+	}
+
+
+	@Override
+	public int currentColumn() {
+		return output.getCurrentColumn();
+	}
+
+
+	@Override
+	public long currentRecord() {
+		return output.getCurrentRecord();
+	}
+
+	@Override
+	public void stop() {
+		stopped = true;
+	}
+
+	@Override
+	public boolean isStopped() {
+		return stopped;
+	}
+}
